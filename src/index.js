c72b745ec891d38d8d468bd846f5dc8fb107556f
kaylyntst <kaylyn.tanton@ucalgary.ca>
2023-03-08 11:19:28 -0700

    initial

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/notes" element={<App />}></Route>
+          <Route exact path="/notes/:id" element={<App />}></Route>
+          <Route exact path="/notes/:id/edit" element={<App />}></Route>
+          <Route exact path="/" element={<Navigate to="/notes" />} />
+        </Routes>
+      </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
